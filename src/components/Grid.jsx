2ac9d6e9eb487364c3c5c1bb5580d8d20ed66fe6
saikatxshrey <saikatxshrey@gmail.com>
2022-06-05 01:34:44 +0530

    letters color

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -5,6 +5,10 @@ const Grid = ({ currentGuess, guesses, turn }) => {
   return (
     <div>
       {guesses.map((guess, i) => {
+        if (turn === i) {
+          return <Row key={i} currentGuess={currentGuess} />;
+        }
+
         return <Row key={i} guess={guess} />;
       })}
     </div>
