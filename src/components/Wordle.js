7c242b9302b2b0ec009cbb7760a599a1010b60c3
saikatxshrey <saikatxshrey@gmail.com>
2022-06-04 17:15:00 +0530

    work test

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -4,13 +4,18 @@ import useWordle from "../hooks/useWordle";
 
 const Wordle = ({ solution }) => {
   // hooks
-  const { currentGuess, handleKeyup } = useWordle(solution);
+  const { turn, currentGuess, guesses, isCorrect, handleKeyup } =
+    useWordle(solution);
 
   useEffect(() => {
     window.addEventListener("keyup", handleKeyup);
 
     return () => window.removeEventListener("keyup", handleKeyup);
   }, [handleKeyup]);
+
+  useEffect(() => {
+    console.log(guesses, turn, isCorrect);
+  }, [guesses, turn, isCorrect]);
 
   return <div>{currentGuess}</div>;
 };
